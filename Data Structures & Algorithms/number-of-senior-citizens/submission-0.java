d2c616f1784de82a0cde84b131af5e15a05dd7ab
tejasdhage <41424142+tejasdhage@users.noreply.github.com>
2026-04-22 11:17:57 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countSeniors(String[] details) {
+
+        int cnt = 0;
+        for (int i = 0; i < details.length; i++) {
+            String currPass = details[i];
+
+            int age = Integer.parseInt(currPass.substring(11, 13));
+
+            if (age > 60) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
